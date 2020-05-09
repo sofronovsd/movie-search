63c8d5bb94ff63f9b17768e133d6b4324536003f
Semyon Sofronov <sofronov.semyon@gmail.com>
2020-05-09 16:55:27 +0400

    init: setup environment

---
+++ movie-search/webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = () => {
+
+    const config = {
+        devtool: 'source-map',
+        entry: './src/script.js',
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'script.js'
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader', 'sass-loader']
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader'
+                        },
+                    ],
+                }
+            ]
+        },
+
+        plugins: [
+            new CleanWebpackPlugin(),
+            // new FaviconsWebpackPlugin('./src/img/big.jpg'),
+            new CopyPlugin([
+                {
+                    from: './src/img', to: './img'
+                },
+                // {
+                //     from: './src/audio', to: './audio'
+                // },
+                // {
+                //     from: './src/icon', to: ''
+                // }
+            ]),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            }),
+        ],
+
+        devServer: {
+            port: 8080,
+            contentBase: path.join(__dirname, 'dist'),
+        }
+
+    };
+
+    return config;
+};
